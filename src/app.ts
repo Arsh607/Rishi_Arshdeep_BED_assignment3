a8e1e63accd9bd6439bdb992b3bcc78323efd6c4
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-02-21 01:24:56 -0600

    feat: Set up a basic health check.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, {Express, Request, Response} from 'express';
 import morgan from 'morgan';
+import { HTTP_STATUS } from './constants/httpsConstants';
+import healthRouter from './api/v1/routes/healthRoute';
 
 const app: Express = express();
 app.use(morgan('combined'));
+app.use('/', healthRouter);
 
 export default app;
