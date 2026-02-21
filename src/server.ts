d1c482089efbb7726d0e4c3cd743b021697a1670
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-02-21 00:58:46 -0600

    feat: Set up the server and updated the script.

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import {Server} from 'http';
+import app from './app';
+
+const PORT: string | number = process.env.port || 3000;
+const server: Server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export {server};
