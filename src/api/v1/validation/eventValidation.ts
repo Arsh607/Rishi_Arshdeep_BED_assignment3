15fc8e7c5dcf518de5c8105dcf7956a255c8447d
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-02-21 21:46:05 -0600

    feat: Created more schemas for id validation and updating the event.

--- src/api/v1/validation/eventValidation.ts
+++ src/api/v1/validation/eventValidation.ts
@@ -9,3 +9,16 @@ export const createEventSchema = Joi.object({
     category: Joi.string().valid("conference", "workshop", "meetup", "seminar", "general")
     .default("general")
 });
+
+export const eventIDParamSchema = Joi.object({
+    id: Joi.string().required().pattern(/^evt_\d{6}$/)
+});
+
+export const updateEventSchema = Joi.object({
+    name: Joi.string().min(3),
+    date: Joi.date().iso().greater("now"),
+    capacity: Joi.number().integer().min(5),
+    registrationCount: Joi.number().integer().min(0).max(Joi.ref("capacity")),
+    status: Joi.string().valid("active", "cancelled", "completed"),
+    category: Joi.string().valid("conference", "workshop", "meetup", "seminar", "general")
+}).min(1);
