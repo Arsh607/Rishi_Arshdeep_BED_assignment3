ac18f83876e4d1ce1e10e735e2653e43000684d6
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-03-31 12:29:52 -0500

    feat: Set up helmet in app.ts.

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,27 @@ import morgan from 'morgan';
 import { HTTP_STATUS } from './constants/httpConstants';
 import healthRouter from './api/v1/routes/healthRoute';
 import eventRouter from "./api/v1/routes/eventRoute";
+import helmet from 'helmet';
 
 const app: Express = express();
 app.use(morgan('combined'));
 app.use(express.json());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+    frameguard: { action: "deny" },
+    referrerPolicy: { policy: "no-referrer" },
+    hsts:
+      process.env.NODE_ENV === "production"
+        ? {
+            maxAge: 31536000,
+            includeSubDomains: true,
+            preload: true,
+          }
+        : false,
+  })
+);
 app.use('/api/v1/health', healthRouter);
 app.use('/api/v1/events', eventRouter);
 
