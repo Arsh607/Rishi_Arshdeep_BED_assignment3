6ee87f97856e33deab3faa8e1c726f9f25349cee
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-03-31 11:38:34 -0500

    feat: Change server file to use env variables.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import {Server} from 'http';
 import app from './app';
 
-const PORT: string | number = process.env.port || 3000;
+const PORT: string | number = process.env.PORT || 3000;
 const server: Server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
